78fc60c320dcf05b4c631e8bc59e664d35a6c292
manoeldavid <manoeldavid00@gmail.com>
2025-05-10 20:09:46 -0300

    dockerizando a aplicacao mas com mySQL local

--- src/database/database.js
+++ src/database/database.js
@@ -1,9 +1,16 @@
+require('dotenv').config();
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize(`booktrack`, 'root', '1234', {
-    host: 'localhost',
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     dialect: 'mysql',
     logging: false
-});
+  }
+);
 
 module.exports = sequelize;
